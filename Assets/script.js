79df0585ed7f49987b634c922f72a1f33e5b1923
Kevin Smith <falchard@live.com>
2023-05-21 14:55:52 -0700

    Added question generation and scoring

--- Assets/script.js
+++ Assets/script.js
@@ -6,7 +6,15 @@ var highScoreSection = document.getElementById("highScore");
 var initialsForm = document.getElementById("inputScore");
 var timer;
 var countDown = 60;
+var score = 0;
+var answer = 1;
+var questionNum = 0;
 
+var question1 = {
+    body: "What symbol comments a single line in JavaScript?",
+    answer: "//",
+    options: ["*", "!-", "::"],
+}
 
 function viewStart(){
     timerSection.style.display = "none";
@@ -24,6 +32,7 @@ function viewQuestion(){
 }
 function viewEnd(){
     clearTimeout(timer);
+    document.getElementById("score").innerHTML = "Your Final Score is: " + score;
     timerSection.style.display = "none";
     startSection.style.display = "none";
     questionSection.style.display = "none";
@@ -40,9 +49,23 @@ function viewHighScore(){
 
 function startGame(){
     viewQuestion();
+    generateQuestion();
     timerSection.innerHTML = "Time: " + countDown;
     timer = setTimeout(timerCountdown, 1000);
 }
+function generateQuestion(){
+    var optionIndex = 2;
+    answer = Math.floor(Math.random() * 4 + 1);
+    document.getElementById("questionInfo").innerHTML = question1.body;
+    document.getElementById("selection" + answer).innerHTML = question1.answer;
+    for(let i = 1; i < 5; i++){
+        if(i != answer){
+            document.getElementById("selection" + i).innerHTML = question1.options[optionIndex];
+            optionIndex--;
+        }
+    }
+}
+
 function timerCountdown(){
     countDown--;
     timerSection.innerHTML = "Time: " + countDown;
@@ -53,6 +76,14 @@ function timerCountdown(){
     timer = setTimeout(timerCountdown, 1000);
 }
 function submitAnswer(selection){
+    if(selection == answer){
+        score += 10;
+        document.getElementById("answer").innerHTML = "Correct!";
+    }
+    else{
+        document.getElementById("answer").innerHTML = "Wrong.";
+    }
+    document.getElementById("answer").style.display = "block";
     viewEnd();
 }
 
@@ -63,5 +94,7 @@ initialsForm.addEventListener('submit', (event) => {
 
 function restart(){
     countDown = 60;
+    score = 0;
+    document.getElementById("answer").style.display = "none";
     viewStart();
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Challenge4_CodingQuiz
 A timed multiple choice quiz
+
+## Credits
