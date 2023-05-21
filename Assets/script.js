20660f2113d92b3187ac00246b5c3e934b5c0d5c
Kevin Smith <falchard@live.com>
2023-05-21 12:25:09 -0700

    Finished creating a basic layout

---
+++ Assets/script.js
@@ -0,0 +1,9 @@
+var timerSection = document.getElementById("timer");
+var startSection = document.getElementById("start");
+var questionSection = document.getElementById("question");
+var endSection = document.getElementById("end");
+var highScoreSection = document.getElementById("highScore");
+
+function viewHighScore(){
+
+}
