65226be56283532c6bc9aab35c09f4e0ab00ee67
Kevin Smith <falchard@live.com>
2023-05-21 12:51:21 -0700

    Built basic navigation

--- Assets/script.js
+++ Assets/script.js
@@ -3,7 +3,50 @@ var startSection = document.getElementById("start");
 var questionSection = document.getElementById("question");
 var endSection = document.getElementById("end");
 var highScoreSection = document.getElementById("highScore");
+var initialsForm = document.getElementById("inputScore");
 
+
+function viewStart(){
+    timerSection.style.display = "none";
+    startSection.style.display = "block";
+    questionSection.style.display = "none";
+    endSection.style.display = "none";
+    highScoreSection.style.display = "none";
+}
+function viewQuestion(){
+    timerSection.style.display = "block";
+    startSection.style.display = "none";
+    questionSection.style.display = "block";
+    endSection.style.display = "none";
+    highScoreSection.style.display = "none";
+}
+function viewEnd(){
+    timerSection.style.display = "none";
+    startSection.style.display = "none";
+    questionSection.style.display = "none";
+    endSection.style.display = "block";
+    highScoreSection.style.display = "none";
+}
 function viewHighScore(){
+    timerSection.style.display = "none";
+    startSection.style.display = "none";
+    questionSection.style.display = "none";
+    endSection.style.display = "none";
+    highScoreSection.style.display = "block";
+}
 
+function startGame(){
+    viewQuestion();
+}
+function submitAnswer(selection){
+    viewEnd();
+}
+
+initialsForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    viewHighScore();
+});
+
+function restart(){
+    viewStart();
 }
